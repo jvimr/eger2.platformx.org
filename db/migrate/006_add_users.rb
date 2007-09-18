8a56db980fdbb616ec90857c18f7c4bfafeeffb4
vimr <vimr>
2007-09-18 15:02:31 +0000

    *** empty log message ***

--- db/migrate/006_add_users.rb
+++ db/migrate/006_add_users.rb
@@ -2,12 +2,12 @@ class AddUsers < ActiveRecord::Migration
   def self.up
   	create_table :users do |t|
   		t.column :id, :primary_key
-		t.column :user, :string, {:null=>false}
+		t.column :login, :string, {:null=>false}
 		t.column :password, :string, { :null=>true } 
   	end
 	
-	User.create :user=> 'admin'
-	User.create :user=> 'hanny'
+	User.create :login=> 'admin'
+	User.create :login=> 'hanny'
   end
 
   def self.down
