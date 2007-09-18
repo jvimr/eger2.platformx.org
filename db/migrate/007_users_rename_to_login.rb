aebadb5b4c4d834360fc6aff6109787a2662de28
vimr <vimr>
2007-09-18 15:35:03 +0000

    *** empty log message ***

--- db/migrate/007_users_rename_to_login.rb
+++ db/migrate/007_users_rename_to_login.rb
@@ -5,6 +5,12 @@ class UsersRenameToLogin < ActiveRecord::Migration
     User.create :login=> 'admin', :password => 'd65fcf8709ca1a34ab9342566a0b0eabe0e4f83e'
     
     User.create :login=> 'hanny'
+    
+    
+    create_table :nic do |t|
+      t.column :id, :primary_key
+      t.column :text, :string
+    end
   end
 
   def self.down
